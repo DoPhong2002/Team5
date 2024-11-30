c8dfbb06f0479fda4ff93774e3b4696e6f7cfd1e
DoPhong2002 <111572145+DoPhong2002@users.noreply.github.com>
2024-11-30 19:30:36 +0700

    first commit

---
+++ app/src/main/java/com/example/team5/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.team5.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Nghị");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/com/example/team5/ui/setting/SettingViewModel.java
@@ -0,0 +1,19 @@
+package com.example.team5.ui.setting;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SettingViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public SettingViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Phong");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
